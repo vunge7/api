d548cac72a96d87c7f89dbfeb36fa4aa40e9faeb
Binilsonabreu <Binilsonabreu1@gmail.com>
2025-07-15 08:36:52 +0100

    Adição da url principal

--- src/main/java/com/dvml/api/ApiApplication.java
+++ src/main/java/com/dvml/api/ApiApplication.java
@@ -6,7 +6,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class ApiApplication {
+public class
+ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
